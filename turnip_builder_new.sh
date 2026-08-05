bbb521e5614bdc0ea357b1834e74806b3a160e0a
WinterMist010 <85207868+WinterMist010@users.noreply.github.com>
2026-08-05 14:00:17 +0700

    Disable apply_patch function in turnip_builder_new.sh

--- turnip_builder_new.sh
+++ turnip_builder_new.sh
@@ -64,14 +64,14 @@ prepare_workdir(){
 		cd $srcfolder
 }
 
-apply_patch() {
-	echo "Applying patch $1"
-	if ! git apply --check $1; then
-			echo "Failed to apply $1!"
-			exit 1
-		fi
-    	git apply $1
-}
+#apply_patch() {
+#	echo "Applying patch $1"
+#	if ! git apply --check $1; then
+#			echo "Failed to apply $1!"
+#			exit 1
+#		fi
+#    	git apply $1
+#}
 
 # $1 - real branch, $2 - escaped branch name
 build_lib_for_android(){
